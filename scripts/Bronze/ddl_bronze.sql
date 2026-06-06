79a9f298d4a29ba91969c872ae057e0fab9a3209
amols <amol.sangar@gmail.com>
2026-06-06 14:15:40 -0400

    Modify column data types in bronze schema tables
    Updated data types for several columns in the bronze schema tables and removed the existing crm_cust_info table definition.

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -16,7 +16,7 @@ END
 
 CREATE TABLE bronze.erp_loc_a101 (
 	cid VARCHAR(50),
-	centry VARCHAR(50)
+	cntry VARCHAR(50)
 );
 
 print 'Table bronze.erp_loc_a101 created successfully.';
@@ -66,7 +66,7 @@ END
 CREATE TABLE bronze.erp_cust_az12
 (
 	cid VARCHAR(50),
-	bdate date,
+	bdate DATE,
 	gen VARCHAR(50)
 )
 
@@ -122,10 +122,10 @@ CREATE TABLE bronze.crm_prd_info
 prd_id INT,
 prd_key VARCHAR(50),
 prd_nm VARCHAR(100),
-prd_cost DECIMAL(18,2),
+prd_cost INT,
 prd_line VARCHAR(50),
-prd_start_dt DATE,
-prd_end_dt DATE
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
 );
 
 print 'Table bronze.crm_prd_info created successfully.'
@@ -153,40 +153,10 @@ cst_id INT,
 cst_key VARCHAR(50),
 cst_firstname VARCHAR(50),
 cst_lastname VARCHAR(50),
-cst_marital_status VARCHAR(1),
-cst_gndr VARCHAR(1),
+cst_marital_status VARCHAR(10),
+cst_gndr VARCHAR(10),
 cst_create_date DATE
 )
 
 print 'Table bronze.crm_cust_info created successfully.'
---========================================================================================================
-/*
-========================================================================================================
-Create Table- crm_cust_info
-========================================================================================================
-Script purpose:
-Create table to hold customer information in the bronze layer of the data warehouse.
-*/
-
-IF EXISTS (SELECT * FROM sys.tables WHERE name = 'crm_cust_info' AND schema_id = SCHEMA_ID('bronze'))
-BEGIN
-	print 'Table bronze.crm_cust_info already exists. Dropping the table...'
-	DROP TABLE bronze.crm_cust_info;
-	print 'Table bronze.crm_cust_info dropped successfully.'
-END
-
---Create table to hold customer information in the bronze layer of the data warehouse
-CREATE TABLE bronze.crm_cust_info
-(
-cst_id INT,
-cst_key VARCHAR(50),
-cst_firstname VARCHAR(50),
-cst_lastname VARCHAR(50),
-cst_marital_status VARCHAR(1),
-cst_gndr VARCHAR(1),
-cst_create_date DATE
-)
-
-print 'Table bronze.crm_cust_info created successfully.'
-
---========================================================================================================
+--==========================================================================================================
